96ae1d67df4f3650f811df2efb9fb26dc372c132
vaibhav9007 <90597459+vaibhav9007@users.noreply.github.com>
2024-02-11 14:20:01 +0000

    learnt basics

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("hello Javascript")
+const account_Id = 1233
+let account_email = "hello@xyz.abc"
+var account_pass = "vaibhav123"
+mama = "nokam"
+let account_no;
+
+
+/*
+    console.log(account_Id = 2);
+*/
+mama = "vaibhav"
+account_email = "hello@njef.jcc"
+account_pass = "hhbxc:"
+console.log(account_email);
+console.log(account_pass);
+console.log(mama);
+console.table([account_Id,account_email,account_pass,account_no])
